74b3bfd3ce3c722a863233135282b9a9ecfc8d16
Chris Dragan <chris@chris.dragan.name>
2019-07-06 21:58:12 +0100

    toggle zones from browser, add favicon.ico

--- www/app.js
+++ www/app.js
@@ -1,6 +1,17 @@
 
 "use strict";
 
+let enzones = [1,1,1,1,1,1]; // TODO change to all zeroes, load with sysinfo
+let zones = null;
+
+function SetZone(zone, en)
+{
+    zones = enzones.map(function(x) { return x ? 0 : -1; });
+    if (en) zones[zone - 1] = 1;
+    for (let i = 1; i <= zones.length; i++)
+        E("z" + i).setContents(i == zone && en ? "Disable" : "Enable");
+}
+
 function ReadCookie(name)
 {
     const v = RegExp("(?:^|;\\s*)" + name + "=([^;=]*)").exec(document.cookie);
@@ -70,8 +81,20 @@ function ConnectionError(method, url, contents, type, Sent)
 
 function Send(method, url, contents, type, Sent)
 {
-    if (false && url === "/sysinfo") {
-        Sent({status: 200, responseText: '{"sdk":"2.1.0(7106d38)","heap_free":47328,"uptime_us":93688371,"reset_reason":0,"timestamp":1561838292,"cur_time":"Sat Jun 29 19:58:12 2019","timezone":1,"wifi_mode":1,"ip":"192.168.1.15","mac":"BC:DD:C2:24:C8:19"}'});
+    // TODO remove this
+    if (false) {
+        Delay(100, function() {
+            console.log(url);
+            if (url === "/sysinfo") {
+                Sent({status: 200, responseText: '{"sdk":"2.1.0(7106d38)","heap_free":47328,"uptime_us":93688371,"reset_reason":0,"timestamp":1561838292,"cur_time":"Sat Jun 29 19:58:12 2019","timezone":1,"wifi_mode":1,"ip":"192.168.1.15","mac":"BC:DD:C2:24:C8:19"}'});
+            }
+            else if (url.startsWith("/manual?")) {
+                Sent({status: 200});
+            }
+            else {
+                Sent({status: 404});
+            }
+        });
         return;
     }
 
@@ -98,6 +121,8 @@ function Send(method, url, contents, type, Sent)
 
 function OnPageLoad()
 {
+    SetZone();
+
     const table = E("sysinfo").insert("table");
 
     Send("GET", "/sysinfo", null, null, function(request) {
@@ -110,7 +135,8 @@ function OnPageLoad()
         };
         Add("Uptime [s]", parseInt(r.uptime_us) / 1000000.0);
         const reasons = ["Power On", "Watchdog", "Exception", "Soft Watchdog", "Software Reset", "", "Reset"];
-        Add("Reset Reason", reasons[r.reset_reason]);
+        const ridx = r.reset_reason;
+        Add("Reset Reason", ridx >= 0 && ridx < reasons.length ? reasons[r.reset_reason] : "" + ridx);
         Add("IP", r.ip);
         Add("MAC Address", r.mac);
         Add("Current Time", r.cur_time);
@@ -120,5 +146,24 @@ function OnPageLoad()
         }
         Add("SDK Version", r.sdk);
         Add("Heap Free [B]", r.heap_free);
+    });
+}
+
+function ToggleZone(zone)
+{
+    if (typeof zone !== "number") return;
+    if (zone < 1 || zone > 6) return;
+    if (zones[zone - 1] < 0) return;
+
+    const state = zones[zone - 1] ? 0 : 1;
+
+    Send("PUT", "/manual", "{\"zone\":" + zone + ",\"state\":" + state + "}", null, function(request) {
+        if (request.status === 200) {
+            console.log(zone + " " + state);
+            SetZone(zone, state);
+        }
+        else {
+            // TODO report error
+        }
     });
 }
